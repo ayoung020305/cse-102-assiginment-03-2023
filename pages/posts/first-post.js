088089bc92a251d57774f6830e43dfe9c5db37b3
ayoung020305 <ayoung020305@hanyang.ac.kr>
2023-04-17 21:26:10 +0900

    update

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,21 +1,21 @@
 import Link from 'next/link';
-import dynamic from 'next/dynamic';
-
-const Sketch = dynamic(() => import('./CircleCollision'), {
-    ssr: false,
-});
+import BallAnimation from './ball/ball.js';
 
 export default function FirstPost() {
     return (
         <>
             <h1>LATEST UPDATE</h1>
             <h2>
-                why skxkskwldksgsmsrjdi
+                The uploading issue related to ONESTORE has been resolved at March 20th 5 : 55 P.M. and the 9th anniversary update has been completed.
+                We apologize for the inconvenience this has been causing.
+                Thank you for the support.  - TEAM42
+
                 <br />
                 <br />
+
                 <Link href="/">Back to home</Link>
             </h2>
-            <Sketch />
+            <BallAnimation />
         </>
     );
 }
